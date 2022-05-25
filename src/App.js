3f0192c3b662dbb2feb8b4754ae38cb5e8437a40
denisbao <denis.bao@cowmed.com.br>
2022-05-25 09:40:50 -0300

    191-Onwards To A More Realistic Exemple (setting up Firebase)

--- src/App.js
+++ src/App.js
@@ -13,9 +13,8 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://custom-hooks-02-b2aeb-default-rtdb.firebaseio.com/tasks.json'
       );
-
       if (!response.ok) {
         throw new Error('Request failed!');
       }
