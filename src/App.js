bb7a94a6db0fae407a74fe9922bf73d7c89c9b83
denisbao <denis.bao@cowmed.com.br>
2022-05-25 10:24:37 -0300

    193-Using the Custom Http Hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import useHttp from './hooks/use-http';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://custom-hooks-02-b2aeb-default-rtdb.firebaseio.com/tasks.json'
-      );
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+  const transformTasks = (tasksOjs) => {
+    const loadedTasks = [];
+    for (const taskKey in tasksOjs) {
+      loadedTasks.push({ id: taskKey, text: tasksOjs[taskKey].text });
     }
-    setIsLoading(false);
-  };
+    setTasks(loadedTasks);
+  }
+
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp(
+    {url: 'https://custom-hooks-02-b2aeb-default-rtdb.firebaseio.com/tasks.json'},
+    transformTasks
+  )
 
   useEffect(() => {
     fetchTasks();
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+/**
+ * @param {object} requestConfig object {url, method, headers, body} for the http request
+ * @param {() => {}} applyData function that will format the data returned from the request
+ * @returns object containing isLoading, error and sendRequest
+ */
 const useHttp = (requestConfig, applyData) => {
 
   const [isLoading, setIsLoading] = useState(false)
@@ -12,9 +17,9 @@ const useHttp = (requestConfig, applyData) => {
     try {
       const response = await fetch(
          requestConfig.url, {
-            method: requestConfig.method,
-            headers: requestConfig.headers,
-            body: JSON.stringify(requestConfig.body)
+            method: requestConfig.method ? requestConfig.method : 'GET',
+            headers: requestConfig.headers ? requestConfig.headers : {},
+            body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
          }
       )
 
@@ -23,6 +28,7 @@ const useHttp = (requestConfig, applyData) => {
       }
 
       const data = await response.json()
+      console.log(data)
       applyData(data) // function provided by the component that calls this hook
       
     } catch (err) {
